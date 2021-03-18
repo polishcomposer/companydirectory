e3d0ff06241445579ecdcf0a8042f44bb8ba9dfe
Dariusz Swoszowski <67848422+polishcomposer@users.noreply.github.com>
2021-03-18 03:54:56 +0000

    Add files via upload

--- libs/php/config.php
+++ libs/php/config.php
@@ -2,7 +2,7 @@
 	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root"; 
+	$cd_user = "userCompany"; 
 	$cd_password = "sElling@348"; 
 	$cd_dbname = "companydirectory"; 
 
